e8e5469ebb97346d4abf14c623fefae1d3054692
Surbhi Bhadviya <32665847+surbhibh@users.noreply.github.com>
2020-03-23 03:09:20 +0530

    execute Unix command via Nodejs

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const { exec } = require("child_process");
+
+exec("tail -n 10 ./logs.txt", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
